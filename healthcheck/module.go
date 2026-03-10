07082595df4746ecc5db37a3ce8847ba15c29673
Drissa KONE <koneenok@outlook.fr>
2026-03-10 09:06:37 +0100

    feat: enhance health check with detailed response including status, version, and service name

--- healthcheck/module.go
+++ healthcheck/module.go
@@ -2,6 +2,11 @@ package healthcheck
 
 import "go.uber.org/fx"
 
+type healthCheckParams struct {
+	fx.In
+	Info *Info `optional:"true"`
+}
+
 var Module = fx.Options(
 	fx.Provide(
 		newHealthCheck,
